05e11aacbfa9acb320d4519ede86b08e019bcda8
GonJi-sama <tammarit.g@gmail.com>
2025-10-20 12:27:50 +0700

    update

--- app.py
+++ app.py
@@ -21,5 +21,10 @@ def draw(number):
     pattern = mf.myfunc('x', number)
     return f"""<pre style="font-size: 100px; line-height: 1;">{pattern}</pre>"""
 
+@app.route('/sum/<int:x>/<int:y>')
+def sumation(x,y):
+    sum = x + y
+    return f"The result of sum between xx and yy is {sum}"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
